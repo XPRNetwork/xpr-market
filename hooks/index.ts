5224fb20f8c949d0b4f6fb45b09bf44cacb5fe3d
Andrew Park <andrew.park@metalpay.co>
2021-04-06 00:42:26 +0000

    WEB-896:  Share Link for social media

--- hooks/index.ts
+++ hooks/index.ts
@@ -75,3 +75,17 @@ export const useEscapeKeyClose = (close: () => void): void => {
     };
   }, []);
 };
+
+export const useClickAway = (ref, onClickAway: () => void): void => {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClickAway();
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+};
