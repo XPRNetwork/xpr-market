084198e8153b51a005ce73b0ca6ef76ff20154c0
Cindy Sun <c.s.sun5@gmail.com>
2021-04-30 11:21:03 -0700

    lint

--- services/upload.ts
+++ services/upload.ts
@@ -2,16 +2,13 @@ const uploadToIPFS = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append('file', file);
   try {
-    const resultRaw = await fetch(
-      'https://nft.protonchain.com/api/upload',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic 617782dd-7b0a-42c2-ac93-3bba2c569dfd`,
-        },
-        body: formData,
-      }
-    );
+    const resultRaw = await fetch('https://nft.protonchain.com/api/upload', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic 617782dd-7b0a-42c2-ac93-3bba2c569dfd`,
+      },
+      body: formData,
+    });
     const result = await resultRaw.json();
     if (result.success) {
       return result.message.IpfsHash;
