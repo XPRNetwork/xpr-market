8b0f4cb89309e36818a403227af9eb4e18ca8bb0
Cindy Sun <c.s.sun5@gmail.com>
2021-04-30 16:16:12 -0700

    Add error handling

--- services/upload.ts
+++ services/upload.ts
@@ -3,13 +3,17 @@ const uploadToIPFS = async (file: File): Promise<string> => {
   formData.append('file', file);
   try {
     const resultRaw = await fetch(
-      `https://api.protonchain.com/v1/chain/files`,
+      `https://api-dev.protonchain.com/v1/chain/files`,
       {
         method: 'POST',
         body: formData,
       }
     );
     const result = await resultRaw.json();
+
+    if (result.error) {
+      throw new Error(result.message);
+    }
     if (result) {
       return result.IpfsHash;
     }
