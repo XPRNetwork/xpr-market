661fbf6c2d7ece360ceb54c39d9d9d09a5e0ee32
Cindy Sun <c.s.sun5@gmail.com>
2021-04-30 11:14:59 -0700

    Change api url for upload

--- services/upload.ts
+++ services/upload.ts
@@ -2,10 +2,16 @@ const uploadToIPFS = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append('file', file);
   try {
-    const resultRaw = await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
-    });
+    const resultRaw = await fetch(
+      'https://nft-demo.protonchain.com/api/upload',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic 617782dd-7b0a-42c2-ac93-3bba2c569dfd`,
+        },
+        body: formData,
+      }
+    );
     const result = await resultRaw.json();
     if (result.success) {
       return result.message.IpfsHash;
