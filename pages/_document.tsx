7e8fddf3e0b4f38ede751be619ba6822f96cc24d
Syed Jafri <syed.jafri2@ucalgary.ca>
2021-05-02 20:05:46 -0600

    Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+
           {gtag.isProduction && (
             <>
               <script
