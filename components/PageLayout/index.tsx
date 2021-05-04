5e9200c5b00e31758f3a5b46eef15bbf9191734c
Syed Jafri <syed.jafri2@ucalgary.ca>
2021-05-03 18:31:54 -0600

    Rename property to name

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -78,13 +78,9 @@ const PageLayout = ({ title, children }: Props): JSX.Element => {
         />
 
         {/* Open Graph */}
+        <meta name="og:site_name" content={META.siteName} key="ogsitename" />
         <meta
-          property="og:site_name"
-          content={META.siteName}
-          key="ogsitename"
-        />
-        <meta
-          property="og:description"
+          name="og:description"
           content={META.description}
           key="ogdescription"
         />
