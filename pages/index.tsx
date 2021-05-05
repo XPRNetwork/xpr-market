ba2d3c83b10e5c9a40e971e1344d230f7eb39420
Joanna Chen <joanna@metalpay.co>
2021-05-05 12:19:03 -0700

    Update homepage with featured templates grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,21 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-import {
-  Title,
-  CollectionTitle,
-  CollectionTitleRow,
-} from '../styles/Title.styled';
-import { TextButton } from '../components/Button/Button.styled';
+import { Title } from '../styles/Title.styled';
 import PageLayout from '../components/PageLayout';
 import ExploreCard from '../components/ExploreCard';
 import Banner from '../components/Banner';
-import FeaturedGrid from '../components/FeaturedGrid'; // Using FeaturedGrid component to potentially easily swap out with FeaturedCarousel component
 import HomepageStatistics from '../components/HomepageStatistics';
 import { MODAL_TYPES } from '../components/Provider';
-import { useFirebaseFeaturedCollections } from '../services/firebase';
+import { useFirebaseFeaturedTemplates } from '../services/firebase';
+import Grid from '../components/Grid';
 
 const MarketPlace = (): JSX.Element => {
-  const router = useRouter();
-  const featuredCollections = useFirebaseFeaturedCollections();
-  const getCollections = () =>
-    featuredCollections.map(({ collection_name, name, order }) => {
-      const redirectToCollection = () => router.push(`/${collection_name}`);
-      return (
-        <Fragment key={collection_name}>
-          <CollectionTitleRow margin={order === 1 ? '0 0 32px' : '52px 0 32px'}>
-            <CollectionTitle onClick={redirectToCollection}>
-              {name}
-            </CollectionTitle>
-            <TextButton onClick={redirectToCollection}>See all</TextButton>
-          </CollectionTitleRow>
-          <FeaturedGrid collection={collection_name} />
-        </Fragment>
-      );
-    });
+  const featuredTemplates = useFirebaseFeaturedTemplates();
   return (
     <PageLayout>
       <Banner modalType={MODAL_TYPES.CLAIM} />
       <ExploreCard />
       <HomepageStatistics />
-      <Title>Featured Collections 🏆</Title>
-      {getCollections()}
+      <Title>New &amp; Noteworthy</Title>
+      <Grid items={featuredTemplates || []} />
     </PageLayout>
   );
 };
