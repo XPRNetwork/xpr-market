7de5a7296a36519ea7ea43435ca35c3661d4b860
Andrew Park <andrewpark9517@gmail.com>
2021-05-06 11:25:09 -0700

    added enter key support and required text on report

--- components/Modal/ReportModal.tsx
+++ components/Modal/ReportModal.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, KeyboardEvent } from 'react';
 import { useModalContext } from '../Provider';
 import { ReportProps } from '../Provider/ModalProvider';
 import { useState } from 'react';
@@ -28,6 +28,17 @@ export const ReportModal = (): JSX.Element => {
     }
   };
 
+  const submit = () => {
+    // TODO: replace with backend report support endpoint
+    console.log('Report: ', input);
+  };
+
+  const onEnterKey = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' && input) {
+      submit();
+    }
+  };
+
   return (
     <Background onClick={handleBackgroundClick}>
       <ModalBox>
@@ -40,18 +51,26 @@ export const ReportModal = (): JSX.Element => {
         <Description>
           We take reports of harm and abuse seriously. To learn more about our
           standards for member behavior, you can read our{' '}
-          <Link>Community Guidelines</Link>.
+          {
+            // TODO: need to input link once ready
+          }
+          <Link target="_blank" rel="noreferrer">
+            Community Guidelines
+          </Link>
+          .
         </Description>
         <TextArea
           maxLength={1000}
           value={input}
-          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={onEnterKey}
+          onChange={(e) => setInput(e.target.value.trim())}
           placeholder={`Please provide details on why you are concened about this ${type.toLowerCase()}.`}
         />
         <HalfButton
+          disabled={!input}
           fullWidth={isMobile}
           margin="0 0 12px"
-          onClick={() => console.log('report: ', input)}>
+          onClick={submit}>
           Report
         </HalfButton>
       </ModalBox>
